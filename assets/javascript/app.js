3abe1c6e4ab5fd81b67cf1cad75bcadbd8f7b735
marvin lara <laramarvin105@gmail.com>
2019-07-24 17:30:47 -0400

    added functioning js

---
+++ assets/javascript/app.js
@@ -0,0 +1,182 @@
+
+const startButton = document.getElementById("start-Btn")
+const nextButton = document.getElementById("next-Btn")
+const questionsContainerElement = document.getElementById("questions-container")
+
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+
+let shuffledQuestions, currentQuestionIndex
+
+
+
+startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+
+function startGame(){
+    console.log('Start')
+    startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(()=> Math.random() - .5)
+    currentQuestionIndex = 0
+    questionsContainerElement.classList.remove("hide")
+    setNextQuestion()
+    
+    
+    
+    
+  
+}
+
+function setNextQuestion(){
+    resetState()
+    
+    showQuestion (shuffledQuestions[currentQuestionIndex]) 
+    console.log("setquestion")
+
+}
+
+
+function showQuestion(question){
+   
+    questionElement.innerText= question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+
+        if (answer.correct){
+            button.dataset.correct= answer.correct
+        }
+
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+        console.log("showquestion")
+    });    
+    
+}
+
+
+function resetState() {
+    nextButton.classList.add("hide")
+    clearStatusClass(document.body)
+    while (answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+
+    }
+    console.log("reset")
+}
+
+
+
+function selectAnswer(e){
+    const selecetedButton = e.target
+    const correct= selecetedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button =>{
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1){
+        nextButton.classList.remove("hide")
+    
+     } else {
+         startButton.innerText = " Restart"
+         startButton.classList.remove("hide")
+     }
+    
+    nextButton.classList.remove("hide")
+    console.log("answerEvents")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct){
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+
+    }
+    console.log("statusClass")
+}
+
+function clearStatusClass(element){
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+
+}
+
+
+
+const questions = [
+    {
+        question: "Who is the best team in the NFC East",
+        answers: [
+
+            { text: " Eagles", correct: true},
+            { text: " Giants", correct: false},
+            { text: " Redskins", correct: false},
+            { text: " Cowboys", correct: false},
+        ]
+
+        
+    },
+    
+{
+    question: "Which QB CAN'T throw past 30 yards?",
+        answers: [
+
+            { text: " Eli", correct: false},
+            { text: " Dak", correct: true},
+            { text: " Wentz", correct: false},
+            { text: " Alex", correct: false},
+        ]
+
+        
+    },
+
+    {
+        question: "Which movies stars a giant ape?",
+            answers: [
+    
+                { text: " Twilight", correct: false},
+                { text: " Avengers", correct: false},
+                { text: " King Kong", correct: true},
+                { text: " Cinderella", correct:false},
+            ]
+    
+            
+        },
+
+{
+    question: "How many double bonds are there in a benzene ring?",
+        answers: [
+
+            { text: " 1", correct: false},
+            { text: " 2", correct: false},
+            { text: " 3", correct: true},
+            { text: " 4", correct: false},
+        ]
+
+        
+    }
+
+
+]
---
+++ assets/javascript/secondtry.js
@@ -0,0 +1,13 @@
+var startButton = document.getElementById ("start-Btn");
+var nextButton = document.getElementById ("next-Btn");
+var questions = document.getElementById("questions")
+var container = document.getElementById("questions-container")
+var answers = document.getElementById("answer-buttons")
+
+
+
+startButton.onclick= function(){ 
+    
+    open.container = open.questions; 
+
+}
